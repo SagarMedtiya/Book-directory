797b7fbca156b6333ef296893c621882adf68e5f
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-06-25 23:00:58 +0530

    book model

---
+++ backend/models/bookModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    
+    title:{
+        type: String,
+        required: true
+    },
+    image: { type: String, required:false, get:(image)=>{
+        return `${image}`;
+    }},
+    authors:{
+        type: Array,
+        required: false
+    },
+    dateOfPublication:{
+        type:Date,
+        required: false
+    },
+    chapters:{
+        type:Array,
+        required: false,
+    },
+    price:{
+        type:Number,
+        required: false
+    }
+
+
+},{timestamps:true, toJSON:{ getters:true }, id:false});
+
+
+
+const BookDB = mongoose.model('bookdb', schema);
+module.exports = BookDB;
