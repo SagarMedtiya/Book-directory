44abd580ad10f0ae450bea4055c6f107abaa8132
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-06-28 11:37:57 +0530

    fix bugs

--- backend/controller/bookController.js
+++ backend/controller/bookController.js
@@ -1,89 +1,90 @@
 const BookDB = require('../models/bookModel');
+
+const fs = require('fs')
+
+
+
 class bookController{
-    async allBooks(req,res){
+    async allBooks(req,res,next){
         let documents;
-        //mongoose-pagination
+        if(req.params.id){
+            const id = req.params.id;
+            try{
+                documents = await BookDB.findById(id).select('-updatedAt -__v -createdAt').sort({ createdAt: -1});
+            }catch(err){
+                return next(err);
+            }
+            return res.json(documents);
+        }
+        else{
+            try{
+                documents = await BookDB.find().select('-updatedAt -__v -createdAt').sort({ createdAt: -1});
+            }catch(err){
+                return next(err);
+            }
+            return res.json(documents);
+        }
+    };
+    async create(req,res, next){
+        if(!req.body){
+            res.status(400).status({message: 'Content cannot be empty'})
+        }
+        const filepath =req.file.path.replace(/\\/g, '/');
+        const { title, author, dateOfPublication, chapters, price } = req.body;
+        let document ;
         try{
-            documents = await BookDB.find().select('-updatedAt -__v').sort({ createdAt: -1});
+            document = await BookDB.create({
+                title,
+                image: filepath,
+                author,
+                dateOfPublication,
+                chapters,
+                price
+            });
         }catch(err){
             return next(err);
         }
-        return res.json(documents);
-    };
-    async show(req, res, next){
-        let documents;
-        //mongoose-pagination
-        try{
-            documents = await BookDB.findById({_id: req.params.id}).select('-updatedAt -__v').sort({ createdAt: -1});
-        }catch(err){
-            return next(err.message);
-        }
-        return res.json(documents);
-    }
-    async create(req,res){
-        if(!req.body){
-            return res.status(400).send({message: "Content cannot be empty!"})
-        }
-        const {
-            title,
-            image,
-            authors ,
-            dateOfPublication,
-            chapters,
-            price
-        }= req.body;
-        console.log(req.body)
-        const bookExist = BookDB.find(data =>data.title === title);
-        if(bookExist) {
-            return res.send('book already existed')
-        }
-        const book ={
-            title,
-            image,
-            authors,
-            dateOfPublication,
-            chapters,
-            price
-        }
-        await BookDB.create(book)
-            .then(data=>{
-                res.status(200).send({message :"Book added"})
-            })
-            .catch(err=>{
-                res.status(500).send({message:err.message})
-            })
+        res.status(201).json(document);
     };
     async update(req,res){
         if(!req.body){
-            res.status(400).send({message: 'Content cannot be empty'})
+            res.status(400).status({message: 'Content cannot be empty'})
         }
-        const id = req.params.id;
-        BookDB.findByIdAndUpdate(id, req.body, { useFindAndModify: false})
-        .then(data=>{
-            if(!data){
-                res.status(404).send({message: `Cannot Update book with ${id}. `})
-            }else{
-                res.status(200).send({message :"Book updated!"})
-            }
-        })
-        .catch(err=>{
-            res.status(500).send({message : "Error update book information"})
-        })
+        let filepath;
+        if(req.file){
+            filepath = req.file.path.replace(/\\/g, '/');
+        }
+        const { title, author, dateOfPublication, chapters, price } = req.body;
+        let document ;
+        try{
+            document = await BookDB.findOneAndUpdate(({id:req.params.id}),{
+                title,
+                ...(req.file && {image: filepath}),
+                author,
+                dateOfPublication,
+                chapters,
+                price
+            }, {new:true});
+        }catch(err){
+            return next(err);
+        }
+        console.log(document);
+        res.json(document);
     };
-    async delete(req,res){
-        const id = req.params.id;
-        BookDB.findByIdAndDelete(id)
-        .then(data=>{
-            if(!data){
-                res.status(404).send({message:`Cannot delete. Check the ${id}`})
-            }
-            else{
-                res.status(200).send({message:'Book deleted Successfully'})
+    async delete(req,res,next){
+        const document = await BookDB.findOneAndRemove({ _id:req.params.id  });
+        if(!document){
+            return next(new Error('Nothing to delete'));
+        }
+        //delete an image
+        const imagePath = document._doc.image;
+        console.log(imagePath);
+        fs.unlink(`${appRoot}/${imagePath}`,(err)=>{
+            if(err){
+                res.status(500).send({message: 'Server error'})
             }
         })
-        .catch(err=>{
-            res.status(500).send({message:`Cannot delete this book.`})
-        })
+        res.json(document);
     }
 }
 
