1a8887f4e5d729a4c106912458040af429247e27
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-06-28 11:36:01 +0530

    init

--- backend/routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const bookController = require('./controller/bookController')
-
-router.get('/books',bookController.allBooks); //show all the books
-router.get('/books/:id',bookController.show); //show the specific book
-router.post('/books',bookController.create); //create the book record
-router.put('/books/:id',bookController.update); //update the book details
-router.delete('/books/:id',bookController.delete); //delete the book record
-module.exports = router
